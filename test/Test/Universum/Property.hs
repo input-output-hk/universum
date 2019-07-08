0b3c3cf23ae349a25afaed72972d36a82591147e
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-07-08 12:55:52 +1000

    Update stack.yaml and fix tests

--- test/Test/Universum/Property.hs
+++ test/Test/Universum/Property.hs
@@ -2,10 +2,10 @@ module Test.Universum.Property
         ( hedgehogTestTree
         ) where
 
-import Universum 
+import Universum
 
 import Data.List (nub)
-import Hedgehog (Property, Gen, MonadGen, forAll, property, assert, (===))
+import Hedgehog (Property, Gen, forAll, property, assert, (===))
 import Test.Tasty (testGroup, TestTree)
 import Test.Tasty.Hedgehog
 
@@ -23,18 +23,14 @@ hedgehogTestTree :: TestTree
 hedgehogTestTree = testGroup "Tests" [utfProps, listProps, boolMProps]
 
 utfProps :: TestTree
-utfProps = testGroup "utf8 conversion property tests" 
+utfProps = testGroup "utf8 conversion property tests"
     [ testProperty "String to ByteString invertible" prop_StringToBytes
     , testProperty "Text to ByteString invertible" prop_TextToBytes
     , testProperty "ByteString to Text or String invertible" prop_BytesTo
     ]
 
-unicode' :: MonadGen m => m U.Char
-unicode' = do
-    a <- Gen.unicode
-    if U.elem a ['\65534', '\65535']
-    then unicode'
-    else return a
+unicode' :: Gen U.Char
+unicode' = Gen.unicode
 
 utf8String :: Gen U.String
 utf8String = Gen.string (Range.linear 0 10000) unicode'
@@ -48,7 +44,7 @@ utf8Bytes = Gen.utf8 (Range.linear 0 10000) unicode'
 -- "\65534" fails, but this is from BU.toString
 -- > import qualified Data.ByteString.UTF8 as BU
 -- > BU.toString (BU.fromString "\65534") == "\65533"
--- > True 
+-- > True
 
 prop_StringToBytes :: Property
 prop_StringToBytes = property $ do
@@ -74,7 +70,7 @@ prop_BytesTo = property $ do
 -- ordNub
 
 listProps :: TestTree
-listProps = testGroup "list function property tests" 
+listProps = testGroup "list function property tests"
     [ testProperty "Hedgehog ordNub xs == nub xs" prop_ordNubCorrect
     , testProperty "Hedgehog hashNub xs == nub xs" prop_hashNubCorrect
     , testProperty "Hedgehog sortNub xs == sort $ nub xs" prop_sortNubCorrect
